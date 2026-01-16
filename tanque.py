5c0b725507b22297c77e2aa83c7c929580e9955a
Anabel Davila <juanislala2@gmail.com>
2026-01-15 20:39:09 -0500

    clase tanque.py

---
+++ tanque.py
@@ -0,0 +1,11 @@
+class Tanque:
+    def __init__(self):
+        self.nombre = "Escudo Humano"
+        self.vida = 200
+        self.defensa = 50
+        self.velocidad = 10
+
+    def accion_especial(self):
+        return "🛡️ ¡DEFENSA TOTAL! El daño recibido se reduce al 0% en el próximo turno."
+
+heroe_tanque = Tanque()
